fa261d47d87fe19cea0be7a2835df7a686db7a5f
korfantijulia <korfantijulia@gmail.com>
2026-04-28 14:35:07 +0200

    Initializing and configuring a React project

---
+++ src/components/AppButton/AppButton.tsx
@@ -0,0 +1,15 @@
+import "./AppButton.css";
+
+type ButtonProps = {
+  text: string;
+  type: "button" | "submit";
+  onClick: () => void;
+};
+
+export function AppButton({ text, type = "button", onClick }: ButtonProps) {
+  return (
+    <button className="app-button" type={type} onClick={onClick}>
+      {text}
+    </button>
+  );
+}
